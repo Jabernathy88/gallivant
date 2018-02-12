a09ab60b37d8b3f3f528aeacdf9bf359d5ff3bb5
ericstotle93 <elu1993@gmail.com>
2018-02-12 15:00:09 -0500

    Adjusted models to reflect new ERD and MVP

--- db/migrate/20180212175314_create_posts.rb
+++ db/migrate/20180212175314_create_posts.rb
@@ -1,9 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :event
       t.string :title
-      t.string :comment
+      t.string :description
       t.references :user, foreign_key: true
       t.references :city, foreign_key: true
 
