dc4c94c00f309e3395b91864367173916f855119
ericstotle93 <elu1993@gmail.com>
2018-02-12 13:17:46 -0500

    Added user, city mock data to app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :posts, dependent: :destroy
+    has_many :cities, through: :posts
 end
---
+++ db/user_data.rb
@@ -0,0 +1,29 @@
+def get_user_data
+    user_data = {
+    'eric' => {
+        name: 'Eric Lu',
+        photo_url: 'https://source.unsplash.com/A_gTI6Jhsj0/300x300',
+        interests: 'Bacon ipsum dolor amet buffalo tongue bresaola short loin pork belly biltong. Flank pastrami frankfurter meatball pig shank, ham picanha pork belly hamburger. Turducken kielbasa ham hock salami shank picanha tri-tip ribeye fatback sirloin kevin short ribs flank pork belly cupim. Strip steak buffalo chicken corned beef tongue. Meatball short loin tongue ball tip fatback beef hamburger short ribs corned beef. Meatloaf t-bone buffalo, turducken leberkas pork belly sausage tail shank beef andouille drumstick.',
+        isAdmin: false
+    },
+    'jeremy' => {
+        name: 'Jeremy Abernathy',
+        photo_url: 'https://source.unsplash.com/A_gTI6Jhsj0/300x300',
+        interests: 'Bacon ipsum dolor amet buffalo tongue bresaola short loin pork belly biltong. Flank pastrami frankfurter meatball pig shank, ham picanha pork belly hamburger. Turducken kielbasa ham hock salami shank picanha tri-tip ribeye fatback sirloin kevin short ribs flank pork belly cupim. Strip steak buffalo chicken corned beef tongue. Meatball short loin tongue ball tip fatback beef hamburger short ribs corned beef. Meatloaf t-bone buffalo, turducken leberkas pork belly sausage tail shank beef andouille drumstick.',
+        isAdmin: true
+    },
+    'supriya' => {
+        name: 'Supriya Yerramilli',
+        photo_url: 'https://source.unsplash.com/A_gTI6Jhsj0/300x300',
+        interests: 'Bacon ipsum dolor amet buffalo tongue bresaola short loin pork belly biltong. Flank pastrami frankfurter meatball pig shank, ham picanha pork belly hamburger. Turducken kielbasa ham hock salami shank picanha tri-tip ribeye fatback sirloin kevin short ribs flank pork belly cupim. Strip steak buffalo chicken corned beef tongue. Meatball short loin tongue ball tip fatback beef hamburger short ribs corned beef. Meatloaf t-bone buffalo, turducken leberkas pork belly sausage tail shank beef andouille drumstick.',
+        isAdmin: true
+    },
+    'jasen' => {
+        name: 'Jasen Baker',
+        photo_url: 'https://source.unsplash.com/A_gTI6Jhsj0/300x300',
+        interests: 'Bacon ipsum dolor amet buffalo tongue bresaola short loin pork belly biltong. Flank pastrami frankfurter meatball pig shank, ham picanha pork belly hamburger. Turducken kielbasa ham hock salami shank picanha tri-tip ribeye fatback sirloin kevin short ribs flank pork belly cupim. Strip steak buffalo chicken corned beef tongue. Meatball short loin tongue ball tip fatback beef hamburger short ribs corned beef. Meatloaf t-bone buffalo, turducken leberkas pork belly sausage tail shank beef andouille drumstick.',
+        isADmin: false
+    }
+    }
+    user_data
+end
