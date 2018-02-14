ece87480583c12d6e69045bafe2bee32f1b72686
JasenABaker <JasenABaker@gmail.com>
2018-02-13 19:23:41 -0500

    back end fix

--- client/src/components/City.js
+++ client/src/components/City.js
@@ -4,28 +4,40 @@ class City extends Component {
 
     state = {
         city: {},
-        posts: []
+        posts: [],
+        isStateNotSet: true
     }
 
     async componentWillMount() {
 
         const responseCity = await axios.get(`/api/cities/${this.props.match.params.id}`)
         const responsePosts = await axios.get(`/api/cities/${this.props.match.params.id}/posts`)
-        console.log(responsePosts.data)
-        this.setState({city: responseCity.data, posts: responsePosts})
+        // console.log(responsePosts.data)
+        this.setState({city: responseCity.data, posts: responsePosts.data, isStateNotSet: false})
+        
 
     
     }
 
     render() {
         const city = this.state.city
+        console.log(this.state.posts)
+
+        
         return (
             <div>
 
                 <h2>{city.name}</h2>
-
-                <p></p>
-
+                <div>
+                {
+                    this.state.posts.map((post)=> {
+                        <div>
+                        <h3>{post.title}</h3>
+                        <p>{post.description}</p>
+                        </div>
+                    })
+                }
+                </div>
             </div>
 
         )
